da9ce8050d6b95e4efcd82f82addcf7344ac9f06
unknown <phamminhduckl@gmail.com>
2020-05-30 23:22:41 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,112 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import bankOne from "./bankOne"
+import bankTwo from './bankTwo';
 
-function App() {
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      display : bankOne,
+      stringDisplay : "",
+      situation : 1
+
+    }
+    this.playAudioPress = this.playAudioPress.bind(this)
+  }
+
+  componentDidMount(){
+    document.addEventListener("keydown",this.playAudioPress)
+  }
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.playAudioPress);
+  }
+
+  playAudioClick(event)
+  {
+    const number = event.target.id;
+    const index = number.charAt(number.length-1)
+    this.setState({
+      stringDisplay: this.state.display[index].id
+    }
+    )
+    const audioEl = document.getElementsByClassName("clip")[index]
+    audioEl.play()
+  }
+
+  playAudioPress(event){
+    const index = (keyCode) => {
+      switch(keyCode){
+        case 81 :
+          return 0;
+        case 87 :
+          return 1;
+        case 69 :
+          return 2;
+        case 65 :
+          return 3;
+        case 83 :
+          return 4;
+        case 68 :
+          return 5;
+        case 90 :
+          return 6;
+        case 88 :
+          return 7;
+        case 67 :
+          return 8;
+        }
+      }
+    
+    
+    const a = index(event.keyCode);
+    this.setState({
+      stringDisplay : this.state.display[a].id
+    })
+    const audioEl = document.getElementsByClassName("clip")[a]
+    audioEl.play()      
+    }
+
+  changeKey(keyCode){
+    
+
+  }
+
+  changeMode(){
+    this.state.display === bankOne ? this.setState({display: bankTwo}) : this.setState({display: bankOne})
+  }
+
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine">
+      <h1>Drum Machine</h1>
+      <p>Type or Click on buttons to play music</p>
+      <div id="display">
+        <div className="row">
+        <div id = "div-btn0" className="display-child col-m-4"> <button id={this.state.display[0].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>Q<audio id={this.state.display[0].keyTrigger} className="clip" src={this.state.display[0].url}></audio></button></div>
+        <div id = "div-btn1" className="display-child col-m-4"> <button id={this.state.display[1].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>W<audio id={this.state.display[1].keyTrigger} className="clip" src={this.state.display[1].url}></audio></button></div>
+        <div id = "div-btn2" className="display-child col-m-4"> <button id={this.state.display[2].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>E<audio id={this.state.display[2].keyTrigger} className="clip" src={this.state.display[2].url}></audio></button></div>
+        </div>
+        <div className="row">
+        <div id = "div-btn3" className="display-child col-m-4"> <button id={this.state.display[3].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>A<audio id={this.state.display[3].keyTrigger} className="clip" src={this.state.display[3].url}></audio></button></div>
+        <div id = "div-btn4" className="display-child col-m-4"> <button id={this.state.display[4].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>S<audio id={this.state.display[4].keyTrigger} className="clip" src={this.state.display[4].url}></audio></button></div>
+        <div id = "div-btn5" className="display-child col-m-4"> <button id={this.state.display[5].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>D<audio id={this.state.display[5].keyTrigger} className="clip" src={this.state.display[5].url}></audio></button></div>
+        </div>
+        <div className="row">
+        <div id = "div-btn6" className="display-child col-m-4"> <button id={this.state.display[6].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>Z<audio id={this.state.display[6].keyTrigger} className="clip" src={this.state.display[6].url}></audio></button></div>
+        <div id = "div-btn7" className="display-child col-m-4"> <button id={this.state.display[7].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>X<audio id={this.state.display[7].keyTrigger} className="clip" src={this.state.display[7].url}></audio></button></div>
+        <div id = "div-btn8" className="display-child col-m-4"> <button id={this.state.display[8].id} className="drum-pad btn-primary" onClick={this.playAudioClick.bind(this)} onKeyDown={this.playAudioPress.bind(this)}>C<audio id={this.state.display[8].keyTrigger} className="clip" src={this.state.display[8].url}></audio></button></div>
+        </div>
+        <div>{this.state.stringDisplay.substring(0,this.state.stringDisplay.length-1)}</div>
+      </div>
+        <div id="div-controlbutton"><button id="controlbutton" className="btn-info" onClick={this.changeMode.bind(this)}>Change Mode</button></div>
+        <div>{this.state.display === bankOne ? <h3>Bank One</h3> : <h3>Bank Two</h3>}</div>
+        <p>Created by Duc Pham</p>
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/bankOne.js
@@ -0,0 +1,56 @@
+const bankOne = [{
+    num: 0,
+    keyCode: 81,
+    keyTrigger: 'Q',
+    id: 'Heater-10',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' },
+  {
+    num: 1,
+    keyCode: 87,
+    keyTrigger: 'W',
+    id: 'Heater-21',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3' },
+  {
+    num: 2,
+    keyCode: 69,
+    keyTrigger: 'E',
+    id: 'Heater-32',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3' },
+  {
+    num: 3,
+    keyCode: 65,
+    keyTrigger: 'A',
+    id: 'Heater-43',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3' },
+  {
+    num: 4,
+    keyCode: 83,
+    keyTrigger: 'S',
+    id: 'Clap4',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3' },
+  {
+    num: 5,
+    keyCode: 68,
+    keyTrigger: 'D',
+    id: 'Open-HH5',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3' },
+  {
+    num: 6,
+    keyCode: 90,
+    keyTrigger: 'Z',
+    id: "Kick-n'-Hat6",
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3' },
+  {
+    num: 7,
+    keyCode: 88,
+    keyTrigger: 'X',
+    id: 'Kick7',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3' },
+  {
+    num: 8,
+    keyCode: 67,
+    keyTrigger: 'C',
+    id: 'Closed-HH8',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3' }];
+
+export default bankOne;
